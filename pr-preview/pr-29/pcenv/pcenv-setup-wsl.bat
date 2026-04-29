ba231c6582a0dd9a4e968ce653a41b17d2ec2dfc
toshi0806 <toshi0806@users.noreply.github.com>
2026-04-29 09:01:45 +0000

    Deploy preview for PR 29 🛫

---
+++ pr-preview/pr-29/pcenv/pcenv-setup-wsl.bat
@@ -0,0 +1,8 @@
+@echo off
+wsl --install --no-distribution
+echo.
+echo ======================================================
+echo  WSL のインストールが完了しました。
+echo  PC を再起動してから pcenv-setup-all.bat を実行してください。
+echo ======================================================
+pause
